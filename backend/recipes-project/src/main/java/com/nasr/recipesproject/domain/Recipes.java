f7c2c704205d4f14873db2f41b4fbcd24d629d30
mohammad <nasrmohammad4804@gmail.com>
2023-09-17 23:08:24 +0430

    change data type of detail filed of recipes from varchar to text

--- backend/recipes-project/src/main/java/com/nasr/recipesproject/domain/Recipes.java
+++ backend/recipes-project/src/main/java/com/nasr/recipesproject/domain/Recipes.java
@@ -2,10 +2,7 @@ package com.nasr.recipesproject.domain;
 
 import com.nasr.recipesproject.base.domain.BaseEntity;
 import com.nasr.recipesproject.dto.response.FileDto;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,8 +18,12 @@ public class Recipes  extends BaseEntity<Long> {
     public static final String CATEGORY_ID="category_id";
 
     private String title;
+
     private String summary;
+
+    @Column(columnDefinition = "text")
     private String detail;
+
     private String image;
 
     @ManyToOne(fetch = FetchType.LAZY)
